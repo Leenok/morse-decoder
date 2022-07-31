12a912dbc9cc75ccb574306ef7095015e11d5030
Leenok <34567431+Leenok@users.noreply.github.com>
2022-07-31 21:15:52 +0300

    init: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,26 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let masB = [];
+    let itstr = "";
+    for(let i=0; i<expr.length; i= i+10){
+      masB.push(expr.substr(i, 10));
+    }
+  
+    for(let i in masB){
+      let buk ="";
+      let simv = masB[i];
+      
+      for(let t=0; t<10; t+=2){
+        let z = simv.substr(t, 2);
+        if(z == '10'){ buk+=".";};
+        if(z == '11'){ buk+="-";};
+      }
+      itstr += MORSE_TABLE[buk] || " ";
+    }
+  
+    
+  return itstr;
 }
 
 module.exports = {
